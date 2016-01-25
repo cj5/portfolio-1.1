63f8c0905775fc15be3b3f9b8af25bc47b7726d3
cj5 <stenny.cj@hotmail.com>
2016-01-24 20:47:23 -0600

    navicon functional
    also:
    added project images
    some variables and mixins

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+$('.lines-button').click(function() {
+        $(this).toggleClass("close");
+        $('.menu').toggleClass("open");           
+});
+
+    
+
+// $('.lines-button').click( function(){
+//     if ( $(this).hasClass('close open') ) {
+//         $(this).removeClass('close open');
+//     } else {
+//         $('lines-button.close').removeClass('close open');
+//         $(this).addClass('close open');    
+//     }
+// });
+
+
